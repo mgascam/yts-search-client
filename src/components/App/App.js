b282977b36d2fee62752e0f072f40ec143b0139d
Miguel Gasca <mgasca@theworkshop.com>
2018-02-09 17:54:20 +0100

    SearchBar

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 import TorrentList from "../../containers/TorrentList/TorrentList";
 import TorrentDetail from "../../containers/TorrentDetail/TorrentDetail";
+import SearchBar from "../../containers/SearchBar/SearchBar";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+          <SearchBar />
           <TorrentList />
           <TorrentDetail />
       </div>
